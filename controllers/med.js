8c54293ab00386510c267e79e3f2ece1472f38e3
Tara Young <ty18881@gmail.com>
2020-03-27 06:29:39 -0400

    MED Controller - implement index route to display orders owned by the currently logged in Med user

--- controllers/med.js
+++ controllers/med.js
@@ -4,16 +4,27 @@ const moment = require("moment-timezone");
 
 
 const Med = require("../models/users.js");
+const Order = require("../models/order.js");
+
 // const seedData = require("../models/seed_appointment.js");
 
 /**
   * INDEX Route - for Med
+  * Display all orders placed by this user
+  * Query the Order collection by user's unique id.
   */
 
  router.get("/", (req,res) => {
-   console.log('Med Controller - inside index route');
+   
     if (req.session.currentUser) {
-        res.send("GOT TO THE MED INDEX ROUTE");
+
+        Order.find(
+          {requestor_id: req.session.currentUser._id },
+          (error, medOrder) => {
+            res.send(medOrder);
+          }
+        )      
+        
       } else {
           res.redirect("/sessions/new");
       }
